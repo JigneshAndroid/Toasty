bd0026c88af5d2f2c858b575fb0de28dc53f7fb5
Jigneshp <jignesh.padhiyar@zoodmall.com>
2024-09-05 10:38:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toasty/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.toasty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mylibrary.Toaster
+import com.example.toasty.ui.theme.ToastyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToastyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        Toaster.showToast(this, "Hello world")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToastyTheme {
+        Greeting("Android")
+    }
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/Toaster.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary
+
+import android.content.Context
+import android.widget.Toast
+
+public class Toaster {
+    companion object {
+        public fun showToast(context: Context, msg: String) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
